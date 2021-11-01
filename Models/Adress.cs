87c9607be21e2775bd2c0e8ba5b5cb5698afa6c1
Ferran <ferranhendriks@hotmail.com>
2021-11-01 16:32:11 +0100

    WIP SQLite & Models
    WIP SQLite & Models

---
+++ Models/Adress.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialBrothers.Models
+{
+    public class Adress
+    {
+        public string StreetName { get; set; }
+        public int HouseNumber { get; set; }
+        public string Zipcode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,16 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using SocialBrothers.Models;
 
 namespace SocialBrothers
 {
@@ -26,12 +26,14 @@ namespace SocialBrothers
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SocialBrothers", Version = "v1" });
-            });
+
+            services.AddDbContext<SocialBrothersContext>(opt =>
+                                               opt.UseInMemoryDatabase("TodoList"));
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "SocialBrothersApi", Version = "v1" });
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +42,8 @@ namespace SocialBrothers
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SocialBrothers v1"));
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SocialBrothersApi v1"));
             }
 
             app.UseHttpsRedirection();
