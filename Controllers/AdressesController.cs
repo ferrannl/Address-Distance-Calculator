a7ca5247d558cabe8fe050aa4e99d1c6fd19ecae
Ferran <ferranhendriks@hotmail.com>
2021-11-02 13:25:46 +0100

    Get lon and lat fix

--- Controllers/AdressesController.cs
+++ Controllers/AdressesController.cs
@@ -118,7 +118,9 @@ namespace SocialBrothers.Controllers
             using (var client = new HttpClient())
             {
                 string bingkey = "AsOwuPhU8FPK9TfxRKzV2wXAyywiAIM6fDnMlqNgTbQ-fWcUdVfZcr_FSkZGEeYv";
-                var addressLine = "Marskramersgilde";
+                var addressLine = "Marskamersgilde";
+
+                //http://dev.virtualearth.net/REST/v1/Locations?countryRegion={countryRegion}&adminDistrict={adminDistrict}&locality={locality}&postalCode={postalCode}&addressLine={addressLine}&userLocation={userLocation}&userIp={userIp}&usermapView={usermapView}&includeNeighborhood={includeNeighborhood}&maxResults={maxResults}&key={BingMapsKey}
 
                 //TODO - send HTTP requests
                 client.BaseAddress = new Uri("http://dev.virtualearth.net/");
@@ -126,7 +128,7 @@ namespace SocialBrothers.Controllers
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
                 //-----
-                HttpResponseMessage response = await client.GetAsync("REST/v1/Locations?addressLine=" + addressLine + "&key=" + bingkey);
+                HttpResponseMessage response = await client.GetAsync("REST/v1/Locations/" + addressLine + "/?o=json&key=" + bingkey);
                 double lat = 0;
                 double lon = 0;
                 if (response.IsSuccessStatusCode)
@@ -142,6 +144,10 @@ namespace SocialBrothers.Controllers
                         }
                     }
                 }
+                Console.WriteLine(lat);
+                Console.WriteLine(lon);
+                Console.WriteLine("ewa");
+
             }
         }
 
